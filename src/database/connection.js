0e111bd540484e722a006f220bce3d00c07651fb
Atharva <atharvapradeeprao.k20@iiits.in>
2022-11-16 20:06:10 +0530

    first commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 require("dotenv").config();
-const mongoUri = process.env.mongoURI;
+const mongoUri =
+  "mongodb+srv://antons:antons@cluster0.bd0hkwu.mongodb.net/?retryWrites=true&w=majority";
 const connectDB = () => {
   mongoose
     .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
