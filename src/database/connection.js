db6d5da6d2d23425a19e00d994f12f7df94b8342
Atharva <atharvapradeeprao.k20@iiits.in>
2022-11-16 20:15:33 +0530

    first commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const mongoUri = process.env.mongoURI;
 const connectDB = () => {
   mongoose
-    .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(
+      "mongodb+srv://antons:antons@cluster0.bd0hkwu.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
     .then(res => {
       console.log("MongoDB Connected");
     });
