7f2d1a1fe3f7ab0a934ee112449db6c46fee1dbb
Atharva <atharvapradeeprao.k20@iiits.in>
2022-11-19 13:43:07 +0530

    auth: register updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const mongoUri = process.env.mongoURI;
 const connectDB = () => {
   mongoose
-    .connect(
-      "mongodb+srv://antons:antons@cluster0.bd0hkwu.mongodb.net/?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(res => {
       console.log("MongoDB Connected");
     });
